4aeaffbcce38bc1e7e6c71c441dc6907ac65a329
Chris Anderson <chanderson0@gmail.com>
2012-01-14 17:23:30 -0800

    replay

--- public/js/timeboats.js
+++ public/js/timeboats.js
@@ -1,5 +1,12 @@
-var Point, Square, State, Timeboats, timestamp;
-var __bind = function(fn, me){ return function(){ return fn.apply(me, arguments); }; };
+var Command, GameObject, GameObject2D, MouseCommand, Point, Serializable, Square, State, Timeboats, timestamp;
+var __bind = function(fn, me){ return function(){ return fn.apply(me, arguments); }; }, __slice = Array.prototype.slice, __hasProp = Object.prototype.hasOwnProperty, __extends = function(child, parent) {
+  for (var key in parent) { if (__hasProp.call(parent, key)) child[key] = parent[key]; }
+  function ctor() { this.constructor = child; }
+  ctor.prototype = parent.prototype;
+  child.prototype = new ctor;
+  child.__super__ = parent.prototype;
+  return child;
+};
 timestamp = function() {
   return +new Date();
 };
@@ -8,6 +15,16 @@ window.onload = function() {
   canvas = $('#game-canvas')[0];
   context = canvas.getContext('2d');
   game = new Timeboats(context, canvas.width, canvas.height);
+  $("#addbutton").prop("disabled", true);
+  $("#addbutton").click(function() {
+    return game.addClick();
+  });
+  $("#playbutton").click(function() {
+    return game.playClick();
+  });
+  $("#timeslider").change(function() {
+    return game.sliderDrag($("#timeslider").val());
+  });
   canvas.onmousedown = function(e) {
     return game.onMouseDown(e);
   };
@@ -31,7 +48,7 @@ window.onload = function() {
     gdt = gdt + dt;
     while (gdt > game.timestep) {
       gdt = gdt - game.timestep;
-      game.update();
+      game.update(game.timestep);
     }
     rdt = rdt + dt;
     if (rdt > game.renderstep) {
@@ -45,125 +62,320 @@ window.onload = function() {
   return frame();
 };
 Timeboats = (function() {
-  function Timeboats(context, width, height) {
+  function Timeboats(context, width, height, slider) {
     this.context = context;
     this.width = width;
     this.height = height;
+    this.slider = slider;
     this.onMouseMove = __bind(this.onMouseMove, this);
     this.onMouseDown = __bind(this.onMouseDown, this);
     this.timestep = 1 / 60;
     this.renderstep = 1 / 60;
-    this.state = new State();
-    this.square = new Square(100, 100, 50);
-    this.state.addObject(this.square);
-    this.destX = 200;
-    this.destY = 200;
-    this.recording = false;
-    this.playback = false;
-    this.history = [];
-    this.state_num = 0;
+    this.gamestate = "init";
+    this.frame_history = [new State()];
+    this.command_history = [];
+    this.frame_num = 0;
+    this.player_id = 1;
     this.message = 'not recording';
   }
-  Timeboats.prototype.update = function() {
-    if (this.recording) {
-      this.history.push(this.state.clone());
+  Timeboats.prototype.playClick = function() {
+    if (this.gamestate === "init") {
+      return this.updateState("init", "recording");
+    } else if (this.gamestate === "recording") {
+      return this.updateState("recording", "paused");
+    } else if (this.gamestate === "paused") {
+      return this.updateState("paused", "playing");
+    } else if (this.gamestate === "playing") {
+      return this.updateState("playing", "paused");
+    } else if (this.gamestate === "stopped") {
+      return this.updateState("stopped", "playing");
     }
-    if (this.playback) {
-      this.state = this.history[this.state_num++];
-      if (this.state_num > this.history.length) {
-        this.message = 'done playback';
-        this.playback = false;
-        this.state = this.old_state;
-        return this.history = [];
-      }
+  };
+  Timeboats.prototype.addClick = function() {
+    if (this.gamestate !== "stopped") {
+      return this.updateState(this.gamestate, "stopped");
     } else {
-      return this.state.update();
+      return this.updateState(this.gamestate, "recording");
+    }
+  };
+  Timeboats.prototype.updateState = function(oldState, newState) {
+    var player;
+    console.log(oldState, '->', newState);
+    if (newState === "recording") {
+      this.player_id++;
+      player = new Square(100, 100, 50);
+      this.frame_history[this.frame_num].addObject(this.player_id, player);
+      this.gamestate = "recording";
+      $("#playbutton").html("Stop");
+      $("#addbutton").html("Rewind");
+      return $("#addbutton").prop("disabled", true);
+    } else if (oldState === "recording" && newState === "paused") {
+      this.frame_num = 0;
+      this.gamestate = "paused";
+      this.updateSlider(this.frame_num);
+      $("#playbutton").html("Play");
+      $("#addbutton").html("Rewind");
+      return $("#addbutton").prop("disabled", false);
+    } else if (newState === "playing") {
+      this.gamestate = "playing";
+      $("#playbutton").html("Pause");
+      return $("#addbutton").html("Rewind");
+    } else if (newState === "paused") {
+      this.gamestate = "paused";
+      $("#playbutton").html("Play");
+      return $("#addbutton").html("Rewind");
+    } else if (newState === "stopped") {
+      this.gamestate = "stopped";
+      this.message = "Done playback.";
+      $("#playbutton").html("Play");
+      $("#addbutton").html("Add New");
+      this.frame_num = 0;
+      return this.updateSlider(this.frame_num);
+    } else {
+      return console.log("couldn't switch state");
+    }
+  };
+  Timeboats.prototype.updateSlider = function(value, max) {
+    if (max == null) {
+      max = -1;
+    }
+    $("#timeslider").prop('value', value);
+    if (max > 0) {
+      return $("#timeslider").prop('max', max);
+    }
+  };
+  Timeboats.prototype.sliderDrag = function(value) {
+    if (this.gamestate === "paused") {
+      return this.frame_num = value;
+    } else {
+      return this.updateState(this.gamestate, "paused");
+    }
+  };
+  Timeboats.prototype.addCommand = function(command) {
+    while (this.frame_num >= this.command_history.length) {
+      this.command_history.push([]);
+    }
+    return this.command_history[this.frame_num].push(command);
+  };
+  Timeboats.prototype.update = function(dt) {
+    var next_state;
+    if (this.gamestate === "recording") {
+      next_state = this.frame_history[this.frame_num].clone();
+      next_state.setCommands(this.command_history[this.frame_num] || []);
+      next_state.update(dt);
+      this.frame_num++;
+      if (this.frame_history.length > this.frame_num) {
+        this.frame_history[this.frame_num] = next_state;
+      } else {
+        this.frame_history.push(next_state);
+      }
+      return this.updateSlider(this.frame_num, this.frame_history.length - 1);
+    } else if (this.gamestate === "playing") {
+      this.frame_num++;
+      this.updateSlider(this.frame_num);
+      if (this.frame_num >= this.frame_history.length) {
+        return this.updateState("playing", "stopped");
+      }
+    } else if (this.gamestate === "paused") {
+      return this.state = this.frame_history[this.frame_num];
     }
   };
   Timeboats.prototype.draw = function() {
     this.context.clearRect(0, 0, this.width + 1, this.height + 1);
-    this.state.draw(this.context);
+    this.frame_history[this.frame_num].draw(this.context);
     return this.context.fillText(this.message, 10, 30);
   };
-  Timeboats.prototype.onMouseDown = function(e) {
-    if (this.recording) {
-      this.message = 'playback';
-      this.recording = false;
-      this.playback = true;
-      this.old_state = this.state;
-      return this.state_num = 0;
-    } else if (this.playback) {
-      this.message = 'cancelled playback';
-      this.playback = false;
-      this.state = this.old_state;
-      return this.history = [];
-    } else {
-      this.message = 'recording';
-      return this.recording = true;
-    }
-  };
+  Timeboats.prototype.onMouseDown = function(e) {};
   Timeboats.prototype.onMouseMove = function(e) {
-    this.square.destx = e[0];
-    return this.square.desty = e[1];
+    var command;
+    if (this.gamestate === "recording") {
+      command = new MouseCommand(this.player_id, e[0], e[1]);
+      return this.addCommand(command);
+    }
   };
   return Timeboats;
 })();
+Serializable = (function() {
+  var __type;
+  __type = 'Serializable';
+  function Serializable() {
+    this.__type = this.__type;
+  }
+  Serializable.buildMap = function() {
+    var id, key, map, maps, res, val;
+    maps = 1 <= arguments.length ? __slice.call(arguments, 0) : [];
+    res = {};
+    for (id in maps) {
+      map = maps[id];
+      for (key in map) {
+        val = map[key];
+        res[key] = val;
+      }
+    }
+    return res;
+  };
+  Serializable.deserialize = function(object, prototypes) {
+    var key, value, _results;
+    if ((object != null) && typeof object === 'object') {
+      if (object.__type != null) {
+        object.__proto__ = prototypes[object.__type].prototype;
+      }
+      _results = [];
+      for (key in object) {
+        value = object[key];
+        _results.push(typeof value === 'object' ? this.deserialize(value, prototypes) : void 0);
+      }
+      return _results;
+    }
+  };
+  return Serializable;
+})();
+GameObject = (function() {
+  __extends(GameObject, Serializable);
+  GameObject.prototype.__type = 'GameObject';
+  function GameObject() {
+    GameObject.__super__.constructor.apply(this, arguments);
+  }
+  GameObject.prototype.clone = function() {
+    return new GameObject();
+  };
+  GameObject.prototype.draw = function(context) {};
+  GameObject.prototype.update = function(dt) {};
+  GameObject.prototype.leave = function(callback) {
+    return callback();
+  };
+  return GameObject;
+})();
+GameObject2D = (function() {
+  __extends(GameObject2D, GameObject);
+  GameObject2D.prototype.__type = 'GameObject2D';
+  function GameObject2D(x, y, vx, vy, rotation) {
+    this.x = x != null ? x : 0;
+    this.y = y != null ? y : 0;
+    this.vx = vx != null ? vx : 0;
+    this.vy = vy != null ? vy : 0;
+    this.rotation = rotation != null ? rotation : 0;
+    GameObject2D.__super__.constructor.call(this, this.state);
+  }
+  GameObject2D.prototype.clone = function() {
+    return new GameObject2D(this.x, this.y, this.vx, this.vy, this.rotation);
+  };
+  GameObject2D.prototype.setPos = function(x, y) {
+    this.x = x;
+    return this.y = y;
+  };
+  GameObject2D.prototype.setVel = function(vx, vy) {
+    this.vx = vx;
+    this.vy = vy;
+    if (this.vx !== 0 || this.vy !== 0) {
+      return this.rotation = Point.getAngle(this.vx, this.vy);
+    }
+  };
+  GameObject2D.prototype.update = function(dt) {
+    var newPos;
+    newPos = Point.add(this.x, this.y, this.vx * dt, this.vy * dt);
+    return this.setPos(newPos.x, newPos.y);
+  };
+  return GameObject2D;
+})();
 State = (function() {
+  __extends(State, Serializable);
+  State.prototype.__type = 'State';
   function State() {
-    this.objects = [];
+    this.objects = {};
+    this.commands = [];
   }
   State.prototype.clone = function() {
-    var object, st, _fn, _i, _len, _ref;
+    var id, object, st, _ref;
     st = new State();
     _ref = this.objects;
-    _fn = function(object) {
-      return st.objects.push(object.clone());
-    };
-    for (_i = 0, _len = _ref.length; _i < _len; _i++) {
-      object = _ref[_i];
-      _fn(object);
+    for (id in _ref) {
+      object = _ref[id];
+      st.objects[id] = object.clone();
     }
     return st;
   };
-  State.prototype.addObject = function(object) {
-    return this.objects.push(object);
+  State.prototype.setCommands = function(commands) {
+    return this.commands = commands;
   };
-  State.prototype.update = function() {
-    var object, _i, _len, _ref, _results;
-    _ref = this.objects;
-    _results = [];
+  State.prototype.addObject = function(id, object) {
+    return this.objects[id] = object;
+  };
+  State.prototype.getObject = function(id) {
+    return this.objects[id];
+  };
+  State.prototype.removeObject = function(id) {
+    return this.objects[id].leave(function() {
+      return delete this.objects[id];
+    });
+  };
+  State.prototype.update = function(dt) {
+    var command, id, object, _fn, _i, _len, _ref, _ref2;
+    _ref = this.commands;
+    _fn = __bind(function(command) {
+      return command.apply(this);
+    }, this);
     for (_i = 0, _len = _ref.length; _i < _len; _i++) {
-      object = _ref[_i];
-      _results.push((function(object) {
-        return object.update();
-      })(object));
+      command = _ref[_i];
+      _fn(command);
     }
-    return _results;
+    _ref2 = this.objects;
+    for (id in _ref2) {
+      object = _ref2[id];
+      object.update(dt);
+    }
+    return true;
   };
   State.prototype.draw = function(context) {
-    var object, _i, _len, _ref, _results;
+    var id, object, _ref;
     _ref = this.objects;
-    _results = [];
-    for (_i = 0, _len = _ref.length; _i < _len; _i++) {
-      object = _ref[_i];
-      _results.push(__bind(function(object) {
-        return object.draw(context);
-      }, this)(object));
+    for (id in _ref) {
+      object = _ref[id];
+      object.draw(context);
     }
-    return _results;
+    return true;
   };
   return State;
 })();
+Command = (function() {
+  __extends(Command, Serializable);
+  Command.prototype.__type = 'Command';
+  function Command(id) {
+    this.id = id;
+    Command.__super__.constructor.apply(this, arguments);
+  }
+  Command.prototype.apply = function(state) {};
+  return Command;
+})();
+MouseCommand = (function() {
+  __extends(MouseCommand, Command);
+  MouseCommand.prototype.__type = 'MouseCommand';
+  function MouseCommand(id, destx, desty) {
+    this.id = id;
+    this.destx = destx;
+    this.desty = desty;
+    MouseCommand.__super__.constructor.call(this, this.id);
+  }
+  MouseCommand.prototype.apply = function(state) {
+    var obj;
+    obj = state.getObject(this.id);
+    if (obj != null) {
+      obj.destx = this.destx;
+      return obj.desty = this.desty;
+    }
+  };
+  return MouseCommand;
+})();
 Square = (function() {
+  __extends(Square, GameObject2D);
+  Square.prototype.__type = 'Square';
   function Square(x, y, size) {
     this.x = x;
     this.y = y;
     this.size = size;
-    this.rotation = 0;
-    this.vx = 0;
-    this.vy = 0;
-    this.destx = 200;
-    this.desty = 200;
+    Square.__super__.constructor.call(this, this.x, this.y);
+    this.destx = this.x;
+    this.desty = this.y;
   }
   Square.prototype.clone = function() {
     var sq;
@@ -175,24 +387,20 @@ Square = (function() {
     sq.desty = this.desty;
     return sq;
   };
-  Square.prototype.update = function() {
+  Square.prototype.update = function(dt) {
     var dir, dist, to_move;
     dir = Point.subtract(this.destx, this.desty, this.x, this.y);
     dist = Point.getLength(dir.x, dir.y);
-    to_move = Point.normalize(dir.x, dir.y, Math.sqrt(dist) / 2);
+    to_move = Point.normalize(dir.x, dir.y, Math.sqrt(dist) * dt * 1000);
     if (dist < 0.5) {
-      to_move.x = 0;
-      to_move.y = 0;
-      this.x = this.destx;
-      this.y = this.desty;
+      to_move = {
+        x: 0,
+        y: 0
+      };
+      this.setPos(this.destx, this.desty);
     }
-    this.vx = to_move.x;
-    this.vy = to_move.y;
-    this.x += this.vx;
-    this.y += this.vy;
-    if (this.vx !== 0 || this.vy !== 0) {
-      return this.rotation = Point.getAngle(this.vx, this.vy);
-    }
+    this.setVel(to_move.x, to_move.y);
+    return Square.__super__.update.call(this, dt);
   };
   Square.prototype.draw = function(context) {
     context.save();
--- src/timeboats.coffee
+++ src/timeboats.coffee
@@ -11,6 +11,17 @@ window.onload = ->
 
   game = new Timeboats context, canvas.width, canvas.height
 
+  $("#addbutton").prop "disabled", true
+
+  $("#addbutton").click ->
+    game.addClick()
+
+  $("#playbutton").click ->
+    game.playClick()
+
+  $("#timeslider").change ->
+    game.sliderDrag $("#timeslider").val()
+
   canvas.onmousedown = (e) ->
     game.onMouseDown e
   canvas.onmousemove = (e) ->
@@ -33,7 +44,7 @@ window.onload = ->
     gdt = gdt + dt
     while gdt > game.timestep
       gdt = gdt - game.timestep
-      game.update()
+      game.update game.timestep
     
     rdt = rdt + dt
     if rdt > game.renderstep
@@ -49,97 +60,263 @@ window.onload = ->
   frame()
 
 class Timeboats
-  constructor: (@context, @width, @height) ->
+  constructor: (@context, @width, @height, @slider) ->
     @timestep = 1 / 60
     @renderstep = 1 / 60
 
-    @state = new State()
-    @square = new Square(100, 100, 50)
-    @state.addObject @square
+    @gamestate = "init"
+    
+    @frame_history = [new State()]
+    @command_history = []
+    @frame_num = 0
 
-    @destX = 200
-    @destY = 200
-
-    @recording = false
-    @playback = false
-    @history = []
-    @state_num = 0
+    @player_id = 1
 
     @message = 'not recording'
 
-  update: ->
-    if @recording
-      @history.push @state.clone()
+  playClick: ->
+    if @gamestate == "init"
+      @updateState "init", "recording"
+    else if @gamestate == "recording"
+      @updateState "recording", "paused"
+    else if @gamestate == "paused"
+      @updateState "paused", "playing"
+    else if @gamestate == "playing"
+      @updateState "playing", "paused"
+    else if @gamestate == "stopped"
+      @updateState "stopped", "playing"
 
-    if @playback
-      @state = @history[@state_num++]
-      if @state_num > @history.length
-        @message = 'done playback'
-        @playback = false
-        @state = @old_state
-        @history = []
+  addClick: ->
+    if @gamestate != "stopped"
+      @updateState @gamestate, "stopped"
     else
-      @state.update()
+      @updateState @gamestate, "recording"
+
+  updateState: (oldState, newState) ->
+    console.log oldState, '->', newState
+    if newState == "recording"
+      @player_id++
+      player = new Square(100, 100, 50)
+      @frame_history[@frame_num].addObject @player_id, player
+      @gamestate = "recording"
+      $("#playbutton").html "Stop"
+      $("#addbutton").html "Rewind"
+      $("#addbutton").prop "disabled", true
+    else if oldState == "recording" and newState == "paused"
+      @frame_num = 0
+      @gamestate = "paused"
+      @updateSlider(@frame_num)
+      $("#playbutton").html "Play"
+      $("#addbutton").html "Rewind"
+      $("#addbutton").prop "disabled", false
+    else if newState == "playing"
+      @gamestate = "playing"
+      $("#playbutton").html "Pause"
+      $("#addbutton").html "Rewind"
+    else if newState == "paused"
+      @gamestate = "paused"
+      $("#playbutton").html "Play"
+      $("#addbutton").html "Rewind"
+    else if newState == "stopped"
+      @gamestate = "stopped"
+      @message = "Done playback."
+      $("#playbutton").html "Play"
+      $("#addbutton").html "Add New"
+      @frame_num = 0
+      @updateSlider(@frame_num)
+    else
+      console.log "couldn't switch state"
+
+  updateSlider: (value, max = -1) ->
+    $("#timeslider").prop 'value', value
+
+    if max > 0
+      $("#timeslider").prop 'max', max
+
+  sliderDrag: (value) ->
+    if @gamestate == "paused" 
+      @frame_num = value
+    else
+      @updateState @gamestate, "paused"
+
+  addCommand: (command) ->
+    while @frame_num >= @command_history.length
+      @command_history.push []
+    
+    @command_history[@frame_num].push command
+
+  update: (dt) ->
+    if @gamestate == "recording"
+      next_state = @frame_history[@frame_num].clone()
+      next_state.setCommands (@command_history[@frame_num] || [])
+      next_state.update dt
+
+      @frame_num++
+      if @frame_history.length > @frame_num
+        @frame_history[@frame_num] = next_state
+      else
+        @frame_history.push next_state
+      
+      @updateSlider(@frame_num, @frame_history.length - 1)
+    else if @gamestate == "playing"
+      @frame_num++
+      @updateSlider(@frame_num)
+
+      if @frame_num >= @frame_history.length
+        @updateState "playing", "stopped"
+
+    else if @gamestate == "paused"
+      @state = @frame_history[@frame_num]
 
   draw: ->
     @context.clearRect 0, 0, @width + 1, @height + 1
-
-    @state.draw @context
-
+    @frame_history[@frame_num].draw @context
     @context.fillText(@message, 10, 30)
 
   onMouseDown: (e) =>
-    if @recording
-      @message = 'playback'
-      @recording = false
-      @playback = true
-      @old_state = @state
-      @state_num = 0
-    else if @playback
-      @message = 'cancelled playback'
-      @playback = false
-      @state = @old_state
-      @history = []
-    else
-      @message = 'recording'
-      @recording = true
 
   onMouseMove: (e) =>
-    @square.destx = e[0]
-    @square.desty = e[1]
+    if @gamestate == "recording"
+      command = new MouseCommand @player_id, e[0], e[1]
+      @addCommand command
 
-class State
+class Serializable
+  __type = 'Serializable'
+
   constructor: ->
-    @objects = []
+    @__type = @__type
+
+  @buildMap: (maps...) ->
+    res = {}
+    for id, map of maps
+      for key, val of map
+        res[key] = val
+    res
+
+  @deserialize: (object, prototypes) ->
+    if object? and typeof object == 'object'
+      if object.__type?
+        object.__proto__ = prototypes[object.__type].prototype
+      
+      for key, value of object
+        if typeof value == 'object'
+          @deserialize(value, prototypes)
+
+class GameObject extends Serializable
+  __type: 'GameObject'
+
+  constructor: () ->
+    super
+
+  clone: ->
+    new GameObject()
+
+  draw: (context) ->
+
+  update: (dt) ->
+    # Override me!
+
+  leave: (callback) ->
+    callback()
+
+class GameObject2D extends GameObject
+  __type: 'GameObject2D'
+
+  constructor: (@x = 0, @y = 0, @vx = 0, @vy = 0, @rotation = 0) ->
+    super @state
+
+  clone: ->
+    new GameObject2D @x, @y, @vx, @vy, @rotation
+
+  setPos: (x, y) ->
+    @x = x
+    @y = y
+
+  setVel: (vx, vy) ->
+    @vx = vx
+    @vy = vy
+
+    if @vx != 0 or @vy != 0
+      @rotation = Point.getAngle(@vx, @vy)
+  
+  update: (dt) ->
+    newPos = Point.add(@x, @y, @vx * dt, @vy * dt)
+    @setPos newPos.x, newPos.y
+
+class State extends Serializable
+  __type: 'State'
+
+  constructor: ->
+    @objects = {}
+    @commands = []
 
   clone: ->
     st = new State()
-    for object in @objects
-      do (object) ->
-        st.objects.push object.clone()
+
+    for id, object of @objects
+      st.objects[id] = object.clone()
+
     return st
   
-  addObject: (object) ->
-    @objects.push object
-  
-  update: ->
-    for object in @objects
-      do (object) ->
-        object.update()
+  setCommands: (commands) ->
+    @commands = commands
+
+  addObject: (id, object) ->
+    @objects[id] = object
+
+  getObject: (id) ->
+    @objects[id]
+
+  removeObject: (id) ->
+    @objects[id].leave () ->
+      delete @objects[id]
+
+  update: (dt) ->
+    for command in @commands
+      do (command) =>
+        command.apply this
+    for id, object of @objects
+      object.update dt
+
+    true
 
   draw: (context) ->
-    for object in @objects
-      do (object) =>
-        object.draw context
+    for id, object of @objects
+      object.draw context
+
+    true
 
 
-class Square
+class Command extends Serializable
+  __type: 'Command'
+
+  constructor: (@id) ->
+    super
+    # Override me!
+
+  apply: (state) ->
+    # Override me!
+
+class MouseCommand extends Command
+  __type: 'MouseCommand'
+
+  constructor: (@id, @destx, @desty) ->
+    super @id
+
+  apply: (state) ->
+    obj = state.getObject @id
+    if obj?
+      obj.destx = @destx
+      obj.desty = @desty
+
+class Square extends GameObject2D
+  __type: 'Square'
+
   constructor: (@x, @y, @size) ->
-    @rotation = 0
-    @vx = 0
-    @vy = 0
-    @destx = 200
-    @desty = 200
+    super @x, @y
+
+    @destx = @x
+    @desty = @y
 
   clone: ->
     sq = new Square @x, @y, @size
@@ -150,25 +327,17 @@ class Square
     sq.desty = @desty
     return sq
 
-  update: ->
+  update: (dt) ->
     dir = Point.subtract @destx, @desty, @x, @y
     dist = Point.getLength dir.x, dir.y
 
-    to_move = Point.normalize dir.x, dir.y, Math.sqrt(dist) / 2
+    to_move = Point.normalize dir.x, dir.y, Math.sqrt(dist) * dt * 1000
     if dist < 0.5
-      to_move.x = 0
-      to_move.y = 0
-      @x = @destx
-      @y = @desty
+      to_move = {x: 0, y: 0}
+      @setPos @destx, @desty
 
-    @vx = to_move.x
-    @vy = to_move.y
-
-    @x += @vx
-    @y += @vy
-
-    if @vx != 0 or @vy != 0
-        @rotation = Point.getAngle(@vx, @vy)
+    @setVel to_move.x, to_move.y
+    super dt
 
   draw: (context) ->
     context.save()
